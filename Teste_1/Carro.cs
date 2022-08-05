2cc05379e153da9f830c25d9729561c4250a6b59
WellintonRafael <wellintonrafaelsantos@gmail.com>
2022-08-05 02:24:02 -0300

    Exercícios

---
+++ Teste_1/Carro.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+public class Carro
+{
+    public string name { get; }
+    public int horsePower { get; }
+    public float price { get; }
+
+    public Carro (string name, int horsePower, float price)
+    {
+        this.name = name;
+        this.horsePower = horsePower;
+        this.price = price;
+    }
+
+    public void vrumm()
+    {
+        Console.WriteLine("Vrum!!! Vrumm!!!");
+    }
+}
+
+
---
+++ Teste_1/Program.cs
@@ -0,0 +1,70 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+class Program
+{
+    static List<Carro> list = new List<Carro>(3);
+    static void Main (string[] args)
+    {
+        TestandoCores();
+       
+    }
+
+    static void TestandoCores()
+    {
+        Console.WriteLine("Digite o nome da cor 'Verde'... Só ela funciona: ");
+        string nomeDaVariavel = Console.ReadLine();
+        bool parsed = Enum.TryParse(nomeDaVariavel, true, out CoresOuCor cor);
+        if (!parsed) return;
+
+        switch (cor) {
+            case CoresOuCor.Verde:
+                Console.WriteLine("Verde é feio!");
+                break;
+            case CoresOuCor.Azul:
+                Console.WriteLine("Azul é feio!");
+                break;
+            case CoresOuCor.Unicórnio:
+                Console.WriteLine("Unicórnio é de princesas!");
+                break;
+        }
+    }
+
+    static void Funcao()
+    {
+        Console.WriteLine("Digite 1 para adicionar um novo carro. Ou 2 para listá-los: ");
+        ConsoleKeyInfo Key = Console.ReadKey();
+        Console.Clear();
+        switch (Key.KeyChar)
+        {
+            case '1':
+                Console.WriteLine("Digite o nome do carro: ");
+                string nomeDoCarro = Console.ReadLine();
+
+                Console.WriteLine("Força do carro: ");
+                int horsePower = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Preço de venda: ");
+                float preco = Convert.ToSingle(Console.ReadLine());
+
+                Carro carro = new Carro (nomeDoCarro, horsePower, preco);
+                list.Add(carro);
+
+
+                break;
+            case '2':
+
+                foreach (Carro car in list) {
+                    Console.WriteLine(car.name);
+                    Console.WriteLine(car.horsePower);
+                    Console.WriteLine(car.price);
+
+                }
+
+
+                break;
+        }
+        Funcao();
+
+    }
+}
+
